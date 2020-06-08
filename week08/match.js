6cab4a3e6b38fbbe45b3d8dee1c54f7458d2fb36
吴利哲 <2593008451@qq.com>
2020-06-08 09:09:12 +0800

    up

---
+++ week08/match.js
@@ -0,0 +1,12 @@
+function match(string) {
+    let foundA = false;
+    for (let c of string) {
+        if (c == "a") {
+            foundA = true
+        } else if (foundA && c == "b") {
+            return true
+        } else {
+            foundA = false
+        }
+    }
+}
